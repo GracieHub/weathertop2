4351b88152f691c24e44828f79b92290d0cd2a19
Glitch (weathertopv4) <none>
2021-09-04 03:26:54 +0000

    👆🍙 Updated with Glitch

---
+++ controllers/dashboard.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const accounts = require ('./accounts.js');
+const logger = require("../utils/logger");
+const stationList = require("../models/station");
+const uuid = require("uuid");
+const station = require('./station.js');
+const stationAnalytics = require('../utils/station-analytics');
+const axios = require("axios");
+const oneCallRequest = `https://api.openweathermap.org/data/2.5/weather?lat={{lat}}&lon={{lon}}&appid=2c9407bebff8d23f3e0083d7eb6fa6d6`
+
+
+const dashboard = {
+  index(request, response) {
+    logger.info("dashboard rendering");
+    const loggedInUser = accounts.getCurrentUser(request);
+    const stationId = request.params.id;
+    logger.debug("Station id = ", stationId);
+    const allStations = stationList.getAllStations();
+    const station = stationList.getStation(stationId);
+    const readingId = request.params.readingid;
+//    const latestReading = stationAnalytics.getLatestReading(station);
+
+//    const stations = stationList.getUserStations(loggedInUser.id)
+
+      for (let i=0; i<allStations.length; i++) {
+      const station = allStations[i];
+        
+      station.maxTemp = stationAnalytics.getMaxTemp(station);
+      station.minTemp = stationAnalytics.getMinTemp(station);
+      station.maxWindSpeed = stationAnalytics.getMaxWindSpeed(station);
+      station.minWindSpeed = stationAnalytics.getMinWindSpeed(station);
+      station.maxPressure = stationAnalytics.getMaxPressure(station);
+      station.minPressure = stationAnalytics.getMinPressure(station);
+      station.weatherIcon = stationAnalytics.weatherIcon(station);
+      station.latestWeather = stationAnalytics.latestWeather(station);
+    }
+
+    const viewData = {
+      title: "Station Dashboard",
+      stations: stationList.getUserStations(loggedInUser.id),
+    };
+    logger.info("about to render", stationList.getAllStations);
+    response.render("dashboard", viewData);
+  },
+
+  deleteStation(request, response) {
+    const stationId = request.params.id;
+    logger.debug(`Deleting Stations ${stationId}`);
+    stationList.removeStation(stationId);
+    response.redirect("/dashboard");
+  },
+
+  addStation(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
+    const newStation = {
+      id: uuid.v1(),
+      userid: loggedInUser.id,
+      title: request.body.title,
+      lat: request.body.lat,
+      lng: request.body.lng,
+      readings: []
+      
+    };
+    logger.debug("Creating a new Station", newStation);
+    stationList.addStation(newStation);
+    response.redirect("/dashboard");
+  },
+
+};
+
+module.exports = dashboard;
---
+++ models/userstation.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const _ = require("lodash");
+const JsonStation = require("./json-station");
+
+const userStation = {
+  store: new JsonStation("./models/userstation.json", { users: [] }),
+  collection: "users",
+
+  getAllUsers() {
+    return this.store.findAll(this.collection);
+  },
+
+  addUser(user) {
+    this.store.add(this.collection, user);
+    this.store.save();
+  },
+
+  getUserById(id) {
+    return this.store.findOneBy(this.collection, { id: id });
+  },
+
+  getUserByEmail(email) {
+    return this.store.findOneBy(this.collection, { email: email });
+  },
+  
+  getUserByPassword(password) {
+    return this.store.findOneBy(this.collection, { password: password });
+  },
+}
+
+module.exports = userStation;
